46535964f94ece422a7ee00ed9a16475f4ab018f
modulus <modulus@spiritofcontradiction.eu>
2021-12-04 12:49:30 +0100

    Day 4, task 2.

---
+++ src/4-2.rs
@@ -0,0 +1,60 @@
+use std::fs::read_to_string;
+
+fn main() {
+    let f = read_to_string("input.txt").unwrap();
+    let mut f = f.split("\n\n");
+    let mut numbers = Vec::new();
+    for i in f.next().unwrap().split(",") {
+        numbers.push(i.parse::<u32>().unwrap());
+    }
+    let mut boards = Vec::new();
+    for i in f {
+        let mut v = Vec::new();
+        for j in i.split_whitespace() {
+            v.push((j.parse::<u32>().unwrap(), false));
+        }
+        boards.push(v);
+    }
+
+    let mut score = 0;
+    for i in &numbers {
+        boards.retain(|board| bingo(board) == false);
+        for b in boards.iter_mut() {
+            for index in 0..b.len() {
+                if b[index].0 == *i {
+                    b[index] = (*i, true);
+                    if bingo(b) {
+                        let mut sum = 0;
+                        for (j, _) in b.iter().filter(|&(_, val)| val == &false) {
+                            sum += j;
+                        }
+                        score = sum * i;
+                        println!("Sum: {}. Number: {}. Product: {}.", sum, i, sum * i);
+                    }
+                }
+            }
+        }
+    }
+}
+
+fn bingo(b: &[(u32, bool)]) -> bool {
+    for i in 0..5 {
+        if b.iter()
+            .enumerate()
+            .filter(|(n, (_, v))| (n >= &(i * 5)) && (n < &((i + 1) * 5)) && *v)
+            .count()
+            == 5
+        {
+            return true;
+        }
+        if b.iter()
+            .enumerate()
+            .filter(|(n, (_, v))| (n % 5 == i) && *v)
+            .count()
+            == 5
+        {
+            return true;
+        }
+    }
+    return false;
+}
