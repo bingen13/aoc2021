f7862d8ad5a494d2eb52baf7878f9881e5d50f68
modulus <modulus@spiritofcontradiction.eu>
2022-12-03 10:30:28 +0100

    Day 2, task 2.

--- src/2-1.rs
+++ src/2-1.rs
@@ -1,33 +1,139 @@
-use std::fs::File;
-use std::io::{self, BufRead};
-use std::path::Path;
+use std::fs::read_to_string;
 
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where
-    P: AsRef<Path>,
-{
-    let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
+#[derive(Debug)]
+enum Play {
+    Rock,
+    Paper,
+    Scissors,
+    U1,
+    U2,
+    U3,
+    Error,
+}
+
+/*
+fn assign(p1: &Play, i: usize) -> Play {
+    if i == 0 {
+        return match p1 {
+            Play::U1 => Play::Rock,
+            Play::U2 => Play::Paper,
+            Play::U3 => Play::Scissors,
+            _ => Play::Error,
+        };
+    }
+    if i == 1 {
+        return match p1 {
+            Play::U1 => Play::Rock,
+            Play::U2 => Play::Scissors,
+            Play::U3 => Play::Paper,
+            _ => Play::Error,
+        };
+    }
+    if i == 2 {
+        return match p1 {
+            Play::U1 => Play::Paper,
+            Play::U2 => Play::Rock,
+            Play::U3 => Play::Scissors,
+            _ => Play::Error,
+        };
+    }
+    if i == 3 {
+        return match p1 {
+            Play::U1 => Play::Paper,
+            Play::U2 => Play::Scissors,
+            Play::U3 => Play::Rock,
+            _ => Play::Error,
+        };
+    }
+    if i == 4 {
+        return match p1 {
+            Play::U1 => Play::Scissors,
+            Play::U2 => Play::Rock,
+            Play::U3 => Play::Paper,
+            _ => Play::Error,
+        };
+    }
+    if i == 5 {
+        return match p1 {
+            Play::U1 => Play::Scissors,
+            Play::U2 => Play::Paper,
+            Play::U3 => Play::Rock,
+            _ => Play::Error,
+        };
+    }
+    return Play::Error;
+}
+*/
+
+fn score(p1: &Play, p2: &Play) -> u64 {
+    let mut score = 0;
+    match p2 {
+        Play::Rock => {
+            score += 1;
+            match p1 {
+                Play::Rock => {
+                    score += 3;
+                }
+                Play::Scissors => {
+                    score += 6;
+                }
+                _ => (),
+            }
+        }
+        Play::Paper => {
+            score += 2;
+            match p1 {
+                Play::Paper => {
+                    score += 3;
+                }
+                Play::Rock => {
+                    score += 6;
+                }
+                _ => (),
+            }
+        }
+        Play::Scissors => {
+            score += 3;
+            match p1 {
+                Play::Scissors => {
+                    score += 3;
+                }
+                Play::Paper => {
+                    score += 6;
+                }
+                _ => (),
+            }
+        }
+        _ => (),
+    }
+    return score;
 }
 
 fn main() {
-    let mut x = 0;
-    let mut y = 0;
-    if let Ok(lines) = read_lines("./input.txt") {
-        for line in lines {
-            if let Ok(ip) = line {
-                let i: Vec<&str> = ip.split(" ").collect();
-                match i[0] {
-                    "forward" => x += i[1].parse::<u32>().unwrap(),
-                    "down" => y += i[1].parse::<u32>().unwrap(),
-                    "up" => y -= i[1].parse::<u32>().unwrap(),
-                    _ => (),
-                }
-            }
+    let f = read_to_string("input.txt").unwrap();
+    let f = f.split('\n');
+    let mut strategy = Vec::new();
+    for i in f {
+        let j: Vec<_> = i.split(' ').collect();
+        if j.len() > 1 {
+            let p1 = match j[0].chars().next().unwrap() {
+                'A' => Play::Rock,
+                'B' => Play::Paper,
+                'C' => Play::Scissors,
+                _ => Play::Error,
+            };
+            let p2 = match j[1].chars().next().unwrap() {
+                'X' => Play::Rock,
+                'Y' => Play::Paper,
+                'Z' => Play::Scissors,
+                _ => Play::Error,
+            };
+            strategy.push((p1, p2));
         }
-    } else {
-        println!("File not found!")
     }
-
-    println!("{}", x * y);
+    let mut result = 0;
+    for (j, k) in strategy.iter() {
+        result += score(j, k);
+    }
+    println!("{:?}", result);
 }
--- src/2-2.rs
+++ src/2-2.rs
@@ -1,37 +1,85 @@
-use std::fs::File;
-use std::io::{self, BufRead};
-use std::path::Path;
+use std::fs::read_to_string;
 
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where
-    P: AsRef<Path>,
-{
-    let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
+#[derive(Debug)]
+enum Play {
+    Rock,
+    Paper,
+    Scissors,
+    U1,
+    U2,
+    U3,
+    Error,
+}
+
+fn score(p1: &Play, p2: &Play) -> u64 {
+    let mut score = 0;
+    match p1 {
+        Play::Rock => match p2 {
+            Play::U1 => {
+                score += 3;
+            }
+            Play::U2 => {
+                score += 4;
+            }
+            Play::U3 => {
+                score += 8;
+            }
+            _ => (),
+        },
+        Play::Paper => match p2 {
+            Play::U1 => {
+                score += 1;
+            }
+            Play::U2 => {
+                score += 5;
+            }
+            Play::U3 => {
+                score += 9;
+            }
+            _ => (),
+        },
+        Play::Scissors => match p2 {
+            Play::U1 => {
+                score += 2;
+            }
+            Play::U2 => {
+                score += 6;
+            }
+            Play::U3 => {
+                score += 7;
+            }
+            _ => (),
+        },
+        _ => (),
+    }
+    return score;
 }
 
 fn main() {
-    let mut x = 0;
-    let mut y = 0;
-    let mut aim = 0;
-    if let Ok(lines) = read_lines("./input.txt") {
-        for line in lines {
-            if let Ok(ip) = line {
-                let i: Vec<&str> = ip.split(" ").collect();
-                match i[0] {
-                    "forward" => {
-                        x += i[1].parse::<u32>().unwrap();
-                        y += aim * i[1].parse::<u32>().unwrap()
-                    }
-                    "down" => aim += i[1].parse::<u32>().unwrap(),
-                    "up" => aim -= i[1].parse::<u32>().unwrap(),
-                    _ => (),
-                }
-            }
+    let f = read_to_string("input.txt").unwrap();
+    let f = f.split('\n');
+    let mut strategy = Vec::new();
+    for i in f {
+        let j: Vec<_> = i.split(' ').collect();
+        if j.len() > 1 {
+            let p1 = match j[0].chars().next().unwrap() {
+                'A' => Play::Rock,
+                'B' => Play::Paper,
+                'C' => Play::Scissors,
+                _ => Play::Error,
+            };
+            let p2 = match j[1].chars().next().unwrap() {
+                'X' => Play::U1,
+                'Y' => Play::U2,
+                'Z' => Play::U3,
+                _ => Play::Error,
+            };
+            strategy.push((p1, p2));
         }
-    } else {
-        println!("File not found!")
     }
-
-    println!("{}", x * y);
+    let mut result = 0;
+    for (j, k) in strategy.iter() {
+        result += score(j, k);
+    }
+    println!("{:?}", result);
 }
