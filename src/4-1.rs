c6fa864ae35ecddb2fa8b75692d15d86f9c640b1
david <modulus@spiritofcontradiction.eu>
2022-12-04 08:05:17 +0100

    Day 4, task 2.

--- src/4-1.rs
+++ src/4-1.rs
@@ -2,57 +2,23 @@ use std::fs::read_to_string;
 
 fn main() {
     let f = read_to_string("input.txt").unwrap();
-    let mut f = f.split("\n\n");
-    let mut numbers = Vec::new();
-    for i in f.next().unwrap().split(",") {
-        numbers.push(i.parse::<u32>().unwrap());
-    }
-    let mut boards = Vec::new();
+    let f = f.split('\n');
+    let mut pairs = 0;
     for i in f {
-        let mut v = Vec::new();
-        for j in i.split_whitespace() {
-            v.push((j.parse::<u32>().unwrap(), false));
-        }
-        boards.push(v);
-    }
-
-    for i in &numbers {
-        for b in boards.iter_mut() {
-            for index in 0..b.len() {
-                if b[index].0 == *i {
-                    b[index] = (*i, true);
-                    if bingo(b) {
-                        let mut sum = 0;
-                        for (j, _) in b.iter().filter(|&(_, val)| val == &false) {
-                            sum += j;
-                        }
-                        println!("Sum: {}. Number: {}. Product: {}.", sum, i, sum * i);
-                        return;
-                    }
-                }
+        if i.len() > 0 {
+            let mut j = i.split(',');
+            let p1 = j.next().unwrap();
+            let p2 = j.next().unwrap();
+            let mut p1 = p1.split('-');
+            let n1 = p1.next().unwrap().parse::<u32>().unwrap();
+            let n2 = p1.next().unwrap().parse::<u32>().unwrap();
+            let mut p2 = p2.split('-');
+            let n3 = p2.next().unwrap().parse::<u32>().unwrap();
+            let n4 = p2.next().unwrap().parse::<u32>().unwrap();
+            if (n1 <= n3) & (n2 >= n4) | ((n3 <= n1) & (n4 >= n2)) {
+                pairs += 1;
             }
         }
     }
-}
-
-fn bingo(b: &[(u32, bool)]) -> bool {
-    for i in 0..5 {
-        if b.iter()
-            .enumerate()
-            .filter(|(n, (_, v))| (n >= &(i * 5)) && (n < &((i + 1) * 5)) && *v)
-            .count()
-            == 5
-        {
-            return true;
-        }
-        if b.iter()
-            .enumerate()
-            .filter(|(n, (_, v))| (n % 5 == i) && *v)
-            .count()
-            == 5
-        {
-            return true;
-        }
-    }
-    return false;
+    println!("{}", pairs);
 }
