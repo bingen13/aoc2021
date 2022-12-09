094d8b12da2a90569523611635afad0978a3fac6
david <modulus@spiritofcontradiction.eu>
2022-12-09 11:07:13 +0100

    AoC2022, day 9, tasks 1 and 2.

--- src/9-1.rs
+++ src/9-1.rs
@@ -1,51 +1,63 @@
+use std::collections::HashSet;
 use std::fs::read_to_string;
+
+fn contact(a: &(i32, i32), b: &(i32, i32)) -> bool {
+    let (d1, d2) = dist(a, b);
+
+    (d1 >= -1) & (d1 <= 1) & (d2 >= -1) & (d2 <= 1)
+}
+
+fn dist(a: &(i32, i32), b: &(i32, i32)) -> (i32, i32) {
+    let (x1, y1) = a;
+    let (x2, y2) = b;
+    (x1 - x2, y1 - y2)
+}
+
+fn move_tail(head: &mut (i32, i32), tail: &mut (i32, i32)) -> () {
+    if !contact(&head, &tail) {
+        let (dx, dy) = dist(&head, &tail);
+        if dx == 0 {
+            tail.1 = (tail.1 + head.1) / 2;
+        } else if dy == 0 {
+            tail.0 = (tail.0 + head.0) / 2;
+        } else if dx.abs() > dy.abs() {
+            tail.0 = (tail.0 + head.0) / 2;
+            tail.1 = head.1;
+        } else if dy.abs() > dx.abs() {
+            tail.1 = (tail.1 + head.1) / 2;
+            tail.0 = head.0;
+        } else {
+            println!("Error!");
+        }
+    }
+}
 
 fn main() {
     let f = read_to_string("input.txt").unwrap();
-    let f = f.split("\n");
-    let mut values = Vec::new();
+    let f = f.split('\n');
+    let mut head: (i32, i32) = (0, 0);
+    let mut tail: (i32, i32) = (0, 0);
+    let mut tail_pos = HashSet::new();
+    tail_pos.insert(tail);
     for i in f {
-        if i.len() == 0 {
-            break;
-        }
-        let mut v = Vec::new();
-        for j in i.chars() {
-            v.push(j.to_digit(10).unwrap());
-        }
-        values.push(v);
-    }
-    let mut sum = 0;
-    for i in 0..values.len() {
-        for j in 0..values[0].len() {
-            if is_min(&values, i, j) {
-                sum += values[i][j] + 1;
+        let i: Vec<_> = i.split(' ').collect();
+        match i[..] {
+            [direction, distance] => {
+                let distance = distance.parse::<i32>().unwrap();
+                for _ in 0..distance {
+                    match direction {
+                        "U" => head.1 += 1,
+                        "D" => head.1 -= 1,
+                        "L" => head.0 -= 1,
+                        "R" => head.0 += 1,
+                        _ => (),
+                    }
+                    move_tail(&mut head, &mut tail);
+                    tail_pos.insert(tail);
+                }
             }
+            _ => (),
         }
     }
-    println!("{}", sum);
-}
-
-fn is_min(v: &Vec<Vec<u32>>, x: usize, y: usize) -> bool {
-    let l = v.len();
-    if x > 0 && v[x][y] >= v[x - 1][y] {
-        return false;
-    }
-    if !is_min_x(&v[x], y) {
-        return false;
-    }
-    if x < (l - 1) && v[x][y] >= v[x + 1][y] {
-        return false;
-    }
-    return true;
-}
-
-fn is_min_x(v: &Vec<u32>, x: usize) -> bool {
-    let l = v.len();
-    if x > 0 && v[x] >= v[x - 1] {
-        return false;
-    }
-    if x < (l - 1) && v[x] >= v[x + 1] {
-        return false;
-    }
-    return true;
+    println!("{}", tail_pos.len());
 }
--- src/9-2.rs
+++ src/9-2.rs
@@ -1,58 +1,74 @@
 use std::collections::HashSet;
 use std::fs::read_to_string;
 
+fn contact(a: &(i32, i32), b: &(i32, i32)) -> bool {
+    let (d1, d2) = dist(a, b);
+    (-1..=1).contains(&d1) & (-1..=1).contains(&d2)
+}
+
+fn dist(a: &(i32, i32), b: &(i32, i32)) -> (i32, i32) {
+    let (x1, y1) = a;
+    let (x2, y2) = b;
+    (x1 - x2, y1 - y2)
+}
+
+fn move_tail(head: &(i32, i32), tail: &mut (i32, i32)) {
+    if !contact(head, tail) {
+        let (dx, dy) = dist(head, tail);
+        if dx == 0 {
+            tail.1 = (tail.1 + head.1) / 2;
+        } else if dy == 0 {
+            tail.0 = (tail.0 + head.0) / 2;
+        } else if dx.abs() > dy.abs() {
+            tail.0 = (tail.0 + head.0) / 2;
+            tail.1 = head.1;
+        } else if dy.abs() > dx.abs() {
+            tail.1 = (tail.1 + head.1) / 2;
+            tail.0 = head.0;
+        } else if (dx.abs() == 2) & (dy.abs() == 2) {
+            tail.0 = (tail.0 + head.0) / 2;
+            tail.1 = (tail.1 + head.1) / 2;
+        } else {
+            println!("Error!");
+        }
+    }
+}
+
 fn main() {
     let f = read_to_string("input.txt").unwrap();
-    let f = f.split("\n");
-    let mut values = HashSet::new();
-    for (i, j) in f.enumerate() {
-        if j.len() == 0 {
-            break;
-        }
-        for (k, l) in j.chars().enumerate() {
-            match l {
-                '0'..='8' => {
-                    values.insert((i, k));
+    let f = f.split('\n');
+    let mut head: (i32, i32) = (0, 0);
+    let mut tail: (i32, i32) = (0, 0);
+    let mut middle = Vec::new();
+    for _ in 0..8 {
+        middle.push((0, 0));
+    }
+    let mut tail_pos = HashSet::new();
+    tail_pos.insert(tail);
+    for i in f {
+        let i: Vec<_> = i.split(' ').collect();
+        if let [direction, distance] = i[..] {
+            let distance = distance.parse::<i32>().unwrap();
+            for _ in 0..distance {
+                match direction {
+                    "U" => head.1 += 1,
+                    "D" => head.1 -= 1,
+                    "L" => head.0 -= 1,
+                    "R" => head.0 += 1,
+                    _ => (),
                 }
-                '9' => (),
-                _ => println!("Error."),
+                for k in 0..middle.len() {
+                    if k == 0 {
+                        move_tail(&head, &mut middle[k]);
+                    } else {
+                        let (tx, ty) = middle[k - 1];
+                        move_tail(&(tx, ty), &mut middle[k]);
+                    }
+                }
+                move_tail(&middle[7], &mut tail);
+                tail_pos.insert(tail);
             }
         }
     }
-    let mut basins = Vec::new();
-    loop {
-        if values.len() == 0 {
-            break;
-        }
-        let mut b = HashSet::new();
-        let val = values.iter().next().unwrap().clone();
-        values.remove(&val);
-        b.insert(val);
-        while let Some(newval) = values.iter().find(|p| belongs(&b, p)).cloned() {
-            values.remove(&newval);
-            b.insert(newval);
-        }
-        basins.push(b);
-    }
-    let mut sizes = Vec::new();
-    for i in basins.iter() {
-        sizes.push(i.len());
-    }
-    sizes.sort();
-    sizes.reverse();
-    println!("{}.", &sizes[0] * &sizes[1] * &sizes[2]);
-}
-
-fn belongs(b: &HashSet<(usize, usize)>, p: &(usize, usize)) -> bool {
-    if p.0 > 0 {
-        if b.contains(&(p.0 - 1, p.1)) {
-            return true;
-        }
-    }
-    if p.1 > 0 {
-        if b.contains(&(p.0, p.1 - 1)) {
-            return true;
-        }
-    }
-    return b.contains(&(p.0 + 1, p.1)) || b.contains(&(p.0, p.1 + 1));
+    println!("{}", tail_pos.len());
 }
