dc70d2eb8c47f089b2f8bdce393dac497343eadf
modulus <modulus@spiritofcontradiction.eu>
2022-12-13 13:04:13 +0100

    AoC2022, day 13, task 2.

--- src/13-1.rs
+++ src/13-1.rs
@@ -1,49 +1,61 @@
 use std::fs::read_to_string;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Clone, Copy)]
 enum Item {
     N(u32),
     O,
     C,
 }
 
-enum Elem {
-    List(Vec<Elem>),
-    Digit(u32),
-    Error,
-}
-
-fn norm(p: &[Item]) -> Elem {
-    let r: Elem;
-    match p {
-        [] => {
-            r = Elem::Error;
+fn order(pair1: &[Item], pair2: &[Item]) -> bool {
+    let mut p1 = Vec::new();
+    let mut p2 = Vec::new();
+    for i in 0..pair1.len() {
+        p1.push(pair1[(pair1.len() - i) - 1]);
+    }
+    for i in 0..pair2.len() {
+        p2.push(pair2[(pair2.len() - i) - 1]);
+    }
+    loop {
+        let item1 = p1.pop().unwrap();
+        let item2 = p2.pop().unwrap();
+        if p1.is_empty() {
+            return true;
         }
-        [n] => {
-            if let Item::N(m) = n {
-                r = Elem::Digit(*m);
-            } else {
-                r = Elem::Error;
+        if p2.is_empty() {
+            return false;
+        }
+        match (item1, item2) {
+            (Item::O, Item::O) => {}
+            (Item::C, Item::C) => {}
+            (Item::N(n1), Item::N(n2)) => {
+                if n1 < n2 {
+                    return true;
+                } else if n1 > n2 {
+                    return false;
+                }
+            }
+            (Item::C, _) => {
+                return true;
+            }
+            (_, Item::C) => {
+                return false;
+            }
+            (Item::N(n), Item::O) => {
+                p1.push(Item::C);
+                p1.push(Item::N(n));
+                p1.push(Item::O);
+                p2.push(item2);
+            }
+            (Item::O, Item::N(n)) => {
+                p2.push(Item::C);
+                p2.push(Item::N(n));
+                p2.push(Item::O);
+                p1.push(item1);
             }
         }
-        [Item::C, _] => {
-            r = Elem::Error;
-        }
-        [Item::O, Item::C] => {
-            r = Elem::List(vec![]);
-        }
-        [Item::O, rest @ .., Item::C] => {
-            r = Elem::List(vec![norm(rest)]);
-        }
-[Item::N(n), rest @ ..] => {
-let mut v = vec![n];
-let v2 = vec![norm(rest)];
-v.extend(v2);
-r = 
-
-
     }
-    Elem::Error
+    false
 }
 
 fn parse(s: &str) -> Vec<Item> {
@@ -83,12 +95,18 @@ fn parse(s: &str) -> Vec<Item> {
 fn main() {
     let f = read_to_string("input.txt").unwrap();
     let f = f.split("\n\n");
+    let mut result = 0;
+    let mut index = 1;
     for i in f {
         let mut i = i.split('\n');
         let p1 = i.next().unwrap();
         let p2 = i.next().unwrap();
         let p1 = parse(p1);
         let p2 = parse(p2);
-        println!("{:?}", p2);
+        if order(&p1, &p2) {
+            result += index;
+        }
+        index += 1;
     }
+    println!("{}", result);
 }
--- src/13-2.rs
+++ src/13-2.rs
@@ -1,96 +1,123 @@
-use std::collections::HashSet;
+use std::cmp::Ordering;
 use std::fs::read_to_string;
 
-// A point.
-#[derive(Debug, Hash, Eq, PartialEq)]
-struct Point {
-    x: u32,
-    y: u32,
+#[derive(Debug, PartialEq, Clone, Copy)]
+enum Item {
+    N(u32),
+    O,
+    C,
 }
 
-impl Point {
-    fn fold(&self, instruction: &(bool, u32)) -> Point {
-        let x;
-        let y;
-        let h = instruction.0;
-        let l = instruction.1;
-        if h {
-            x = self.x;
-            if self.y > l {
-                y = (2 * l) - self.y;
-            } else {
-                y = self.y;
+fn order(pair1: &[Item], pair2: &[Item]) -> bool {
+    let mut p1 = Vec::new();
+    let mut p2 = Vec::new();
+    for i in 0..pair1.len() {
+        p1.push(pair1[(pair1.len() - i) - 1]);
+    }
+    for i in 0..pair2.len() {
+        p2.push(pair2[(pair2.len() - i) - 1]);
+    }
+    loop {
+        let item1 = p1.pop().unwrap();
+        let item2 = p2.pop().unwrap();
+        if p1.is_empty() {
+            return true;
+        }
+        if p2.is_empty() {
+            return false;
+        }
+        match (item1, item2) {
+            (Item::O, Item::O) => {}
+            (Item::C, Item::C) => {}
+            (Item::N(n1), Item::N(n2)) => {
+                if n1 < n2 {
+                    return true;
+                } else if n1 > n2 {
+                    return false;
+                }
             }
-        } else {
-            y = self.y;
-            if self.x > l {
-                x = (2 * l) - self.x;
-            } else {
-                x = self.x;
+            (Item::C, _) => {
+                return true;
+            }
+            (_, Item::C) => {
+                return false;
+            }
+            (Item::N(n), Item::O) => {
+                p1.push(Item::C);
+                p1.push(Item::N(n));
+                p1.push(Item::O);
+                p2.push(item2);
+            }
+            (Item::O, Item::N(n)) => {
+                p2.push(Item::C);
+                p2.push(Item::N(n));
+                p2.push(Item::O);
+                p1.push(item1);
             }
         }
-        Point { x: x, y: y }
     }
+    false
+}
+
+fn parse(s: &str) -> Vec<Item> {
+    let mut digit: Option<u32> = None;
+    let mut v = Vec::new();
+    for i in s.chars() {
+        match i {
+            '[' => v.push(Item::O),
+            ']' => {
+                if let Some(d) = digit {
+                    v.push(Item::N(d));
+                    digit = None;
+                }
+                v.push(Item::C);
+            }
+            ',' => {
+                if let Some(d) = digit {
+                    v.push(Item::N(d));
+                    digit = None;
+                }
+            }
+            '0'..='9' => {
+                if let Some(mut d) = digit {
+                    d *= 10;
+                    d += i.to_digit(10).unwrap();
+                    digit = Some(d);
+                } else {
+                    digit = Some(i.to_digit(10).unwrap());
+                }
+            }
+            _ => println!("Well, fuck."),
+        }
+    }
+    v
 }
 
 fn main() {
     let f = read_to_string("input.txt").unwrap();
-    let mut f = f.split("\n\n");
-    let points = f.next().unwrap();
-    let points = points.split("\n");
-    let instructions = f.next().unwrap();
-    let mut sheet: HashSet<Point> = HashSet::new();
-    for i in points {
-        let mut p = i.split(",");
-        sheet.insert(Point {
-            x: p.next().unwrap().parse::<u32>().unwrap(),
-            y: p.next().unwrap().parse::<u32>().unwrap(),
-        });
+    let f = f.split("\n\n");
+    let mut pairs = Vec::new();
+    let divider1 = parse("[[2]]");
+    let divider2 = parse("[[6]]");
+    pairs.push(divider1.clone());
+    pairs.push(divider2.clone());
+    for i in f {
+        let mut i = i.split('\n');
+        let p1 = i.next().unwrap();
+        let p2 = i.next().unwrap();
+        let p1 = parse(p1);
+        let p2 = parse(p2);
+        pairs.push(p1);
+        pairs.push(p2);
     }
-    let instructions = instructions.split("\n");
-    let mut operations = Vec::new();
-    for i in instructions {
-        for j in i.split_whitespace().last() {
-            if j.len() > 0 {
-                let mut k = j.split("=");
-                let horizontal = match k.next().unwrap() {
-                    "x" => false,
-                    "y" => true,
-                    _ => {
-                        println!("Error.");
-                        false
-                    }
-                };
-                let line = k.next().unwrap().parse::<u32>().unwrap();
-                operations.push((horizontal, line));
-            }
+    pairs.sort_by(|i1, i2| {
+        if order(i1, i2) {
+            Ordering::Less
+        } else {
+            Ordering::Greater
         }
-    }
-    for i in operations.iter() {
-        sheet = sheet.iter().map(|p| p.fold(i)).collect();
-    }
-    printer(&sheet);
-}
-
-fn printer(s: &HashSet<Point>) {
-    let m = s
-        .iter()
-        .map(|p| p.x)
-        .reduce(|a, b| if a > b { a } else { b })
-        .unwrap();
-    let n = s
-        .iter()
-        .map(|p| p.y)
-        .reduce(|a, b| if a > b { a } else { b })
-        .unwrap();
-    for i in 0..=n {
-        for j in 0..=m {
-            if s.contains(&Point { x: j, y: i }) {
-                print!("#");
-            } else {
-                print!(".");
-            }
-        }
-        print!("\n");
-    }
+    });
+    let d1 = pairs.iter().position(|x| x == &divider1).unwrap();
+    let d2 = pairs.iter().position(|x| x == &divider2).unwrap();
+    println!("{}", (d1 + 1) * (d2 + 1));
 }
