35bce3f424b46b812f80d2a6ca6e56dd4eabe4f7
modulus <modulus@spiritofcontradiction.eu>
2022-12-22 10:39:50 +0100

    Day 20, task 1.

--- src/20-1.rs
+++ src/20-1.rs
@@ -1,42 +1,40 @@
-use std::cmp::{max, min};
 use std::fs::read_to_string;
 
-fn order(n: &[(i32, usize)]) {
-    for i in 0..n.len() {
-        print!("{} ", n.iter().find(|p| p.1 == i).unwrap().0);
+fn mix(v: &mut Vec<i64>, p: &mut Vec<usize>) {
+    for i in 0..v.len() {
+        let item = p.iter().position(|&x| x == i).unwrap();
+        p.remove(item);
+        p.insert((item as i64 + v[i]).rem_euclid(p.len() as i64) as usize, i);
     }
-    println!();
+}
+
+fn order(v: &[(usize, i64)]) -> Vec<i64> {
+    const key: i64 = 811589153;
+    let mut result = Vec::new();
+    for i in 0..v.len() {
+        result.push(v.iter().find(|p| p.0 == i).unwrap().1);
+    }
+    result
 }
 
 fn main() {
     let f = read_to_string("input.txt").unwrap();
     let f = f.split('\n');
     let mut numbers = Vec::new();
-    for (i, j) in f.enumerate() {
-        if j.is_empty() {
-            break;
+    for i in f {
+        if let Ok(i) = i.parse::<i64>() {
+            numbers.push(i);
         }
-        numbers.push((j.parse::<i32>().unwrap(), i));
     }
-    let l = numbers.len();
-    order(&numbers);
-    for i in 0..numbers.len() {
-        let n = numbers[i];
-        let newplace = (n.0 + n.1 as i32).rem_euclid(l as i32);
-        for j in 0..(n.0.abs()) % (l as i32) {
-            let p = numbers
-                .iter()
-                .position(|x| x.1 == (n.1 + j as usize + 1) % l)
-                .unwrap();
-//            println!("{}, {}, {}", j, p, numbers[p].0);
-            numbers[p].1 = (n.1 + j as usize) % l;
-        }
-        if n.0 < 0 {
-            numbers[i].1 = (newplace - 1).rem_euclid(l as i32) as usize;
-        } else {
-            numbers[i].1 = newplace as usize;
-        }
-        println!("{:?}", numbers);
-        order(&numbers);
-    }
+    let mut positions: Vec<_> = (0..numbers.len()).collect();
+    mix(&mut numbers, &mut positions);
+    let p = positions
+        .iter()
+        .position(|&x| x == numbers.iter().position(|&x| x == 0).unwrap())
+        .unwrap();
+    let pos1 = numbers[positions[(p + 1000) % positions.len()]];
+    let pos2 = numbers[positions[(p + 2000) % positions.len()]];
+    let pos3 = numbers[positions[(p + 3000) % positions.len()]];
+    let result = pos1 + pos2 + pos3;
+    println!("{}", result);
 }
