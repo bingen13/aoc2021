a4433f51d45852fcb81adef562c0f58507159c80
modulus <modulus@spiritofcontradiction.eu>
2023-12-01 16:48:07 +0100

    AoC 2023, day 1, task 2.

--- src/1-1.rs
+++ src/1-1.rs
@@ -2,17 +2,17 @@ use std::fs::read_to_string;
 
 fn main() {
     let f = read_to_string("input.txt").unwrap();
-    let f = f.split("\n\n");
-    let mut elves = Vec::new();
+    let f = f.split('\n');
+    let mut n = 0;
     for i in f {
-        let mut elf = 0;
-        let i = i.split("\n");
-        for j in i {
-            if j.len() > 1 {
-                elf += j.parse::<u32>().unwrap();
-            }
+        if i.is_empty() {
+            break;
         }
-        elves.push(elf);
+        let mut nums = Vec::new();
+        for j in i.chars().filter(|c| c.is_digit(10)) {
+            nums.push(j.to_digit(10).unwrap());
+        }
+        n += (10 * nums.first().unwrap()) + nums.last().unwrap();
     }
-    print!("Max: {}", elves.iter().max().unwrap());
+    println!("{}", n);
 }
--- src/1-2.rs
+++ src/1-2.rs
@@ -1,20 +1,52 @@
 use std::fs::read_to_string;
 
-fn main() {
-    let f = read_to_string("input.txt").unwrap();
-    let f = f.split("\n\n");
-    let mut elves = Vec::new();
-    for i in f {
-        let mut elf = 0;
-        let i = i.split('\n');
-        for j in i {
-            if j.len() > 1 {
-                elf += j.parse::<u32>().unwrap();
+fn nums_rev(n: &[&str]) -> Vec<String> {
+    let mut v = Vec::new();
+    for i in n {
+        v.push(i.chars().rev().collect::<String>());
+    }
+    v
+}
+
+fn get_first(s: &String, n: &[&str]) -> u32 {
+    for i in 0..s.len() {
+        let s = s.get(i..).unwrap();
+        if s.starts_with(|c: char| c.is_digit(10)) {
+            return s.chars().next().unwrap().to_digit(10).unwrap() as u32;
+        }
+        for (j, k) in n.iter().enumerate() {
+            if s.starts_with(k) {
+                return (j + 1) as u32;
             }
         }
-        elves.push(elf);
     }
-    elves.sort();
-    elves.reverse();
-    print!("Sum: {}.", elves[0] + elves[1] + elves[2]);
+    0
+}
+
+fn main() {
+    let numbers = vec![
+        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    ];
+    let f = read_to_string("input.txt").unwrap();
+    let f = f.split('\n');
+    let mut n = 0;
+    for i in f {
+        if i.is_empty() {
+            break;
+        }
+        let mut nums = Vec::new();
+        let r = i.chars().rev().collect::<String>();
+        let first = get_first(&i.to_string(), &numbers);
+        let last = get_first(
+            &r,
+            &nums_rev(&numbers)
+                .iter()
+                .map(|x| x.as_str())
+                .collect::<Vec<&str>>(),
+        );
+        nums.push(first);
+        nums.push(last);
+        n += (10 * nums.first().unwrap()) + nums.last().unwrap();
+    }
+    println!("{}", n);
 }
