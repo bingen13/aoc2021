9224b2ad46738a9a5989705f4d3b08e875a6b947
modulus <modulus@spiritofcontradiction.eu>
2023-12-05 12:28:49 +0100

    AoC 2022, day 3, task 1.

--- src/3-1.rs
+++ src/3-1.rs
@@ -1,32 +1,57 @@
-use std::collections::HashSet;
+use regex::Regex;
 use std::fs::read_to_string;
 
 fn main() {
     let f = read_to_string("input.txt").unwrap();
     let f = f.split('\n');
-    let mut priors = 0;
-    for i in f {
-        let mut h1 = HashSet::new();
-        let mut h2 = HashSet::new();
-        let l = i.len();
-        if l > 0 {
-            for (j, k) in i.chars().enumerate() {
-                if j < l / 2 {
-                    h1.insert(k);
-                } else {
-                    h2.insert(k);
-                }
-            }
-        }
-        let common = h1.intersection(&h2);
-        for m in common {
-            if m.is_uppercase() {
-                priors += 27;
-            } else {
-                priors += 1;
-            }
-            priors += m.to_lowercase().next().unwrap() as u32 - 'a' as u32;
+    let f = f
+        .filter_map(|s| if s == "" { None } else { Some(s.to_string()) })
+        .collect::<Vec<String>>();
+
+    let re = Regex::new(r"(\d+)").unwrap();
+    let mut numbers = Vec::new();
+    for (i, j) in f.iter().enumerate() {
+        for k in re.captures_iter(j) {
+            numbers.push((
+                i,
+                k.get(1)
+                    .unwrap()
+                    .as_str()
+                    .to_string()
+                    .parse::<u32>()
+                    .unwrap(),
+                k.get(1).unwrap().start(),
+                k.get(1).unwrap().end(),
+            ));
         }
     }
-    println!("{}", priors);
+    let l = f[0].len();
+    let mut total = 0;
+    for n in numbers {
+        let (line, number, start, end) = n;
+        let mut sur = Vec::new();
+        let min = if start == 0 { 0 } else { start - 1 };
+        let max = if end == l { l } else { end + 1 };
+        if line != 0 {
+            for i in f[line - 1][min..max].chars() {
+                sur.push(i);
+            }
+        }
+        if line != l - 1 {
+            for i in f[line + 1][min..max].chars() {
+                sur.push(i);
+            }
+        }
+        if start != 0 {
+            sur.push(f[line][min..].chars().next().unwrap());
+        }
+        if end != l {
+            sur.push(f[line][..end + 1].chars().rev().next().unwrap());
+        }
+        if sur.iter().any(|&c| c != '.') {
+            //            println!("{}: {:?}", number, sur);
+            total += number;
+        }
+    }
+    println!("{}", total);
 }
