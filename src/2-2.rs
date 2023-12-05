daec4d2be2fcb3fa40bf3acb50b75d9bc2fcca09
modulus <modulus@spiritofcontradiction.eu>
2023-12-05 10:14:12 +0100

    AoC 2023, day 2, task 2.

--- src/2-2.rs
+++ src/2-2.rs
@@ -1,85 +1,52 @@
 use std::fs::read_to_string;
 
-#[derive(Debug)]
-enum Play {
-    Rock,
-    Paper,
-    Scissors,
-    U1,
-    U2,
-    U3,
-    Error,
-}
-
-fn score(p1: &Play, p2: &Play) -> u64 {
-    let mut score = 0;
-    match p1 {
-        Play::Rock => match p2 {
-            Play::U1 => {
-                score += 3;
-            }
-            Play::U2 => {
-                score += 4;
-            }
-            Play::U3 => {
-                score += 8;
-            }
-            _ => (),
-        },
-        Play::Paper => match p2 {
-            Play::U1 => {
-                score += 1;
-            }
-            Play::U2 => {
-                score += 5;
-            }
-            Play::U3 => {
-                score += 9;
-            }
-            _ => (),
-        },
-        Play::Scissors => match p2 {
-            Play::U1 => {
-                score += 2;
-            }
-            Play::U2 => {
-                score += 6;
-            }
-            Play::U3 => {
-                score += 7;
-            }
-            _ => (),
-        },
-        _ => (),
-    }
-    return score;
-}
-
 fn main() {
     let f = read_to_string("input.txt").unwrap();
     let f = f.split('\n');
-    let mut strategy = Vec::new();
-    for i in f {
-        let j: Vec<_> = i.split(' ').collect();
-        if j.len() > 1 {
-            let p1 = match j[0].chars().next().unwrap() {
-                'A' => Play::Rock,
-                'B' => Play::Paper,
-                'C' => Play::Scissors,
-                _ => Play::Error,
-            };
-            let p2 = match j[1].chars().next().unwrap() {
-                'X' => Play::U1,
-                'Y' => Play::U2,
-                'Z' => Play::U3,
-                _ => Play::Error,
-            };
-            strategy.push((p1, p2));
+    let mut total = 0;
+    'lineloop: for i in f {
+        if i.is_empty() {
+            break;
         }
+        let mut s = i.split(": ");
+        let game = s
+            .next()
+            .unwrap()
+            .split(" ")
+            .nth(1)
+            .unwrap()
+            .parse::<u32>()
+            .unwrap();
+        let s = s.next().unwrap().split("; ");
+let (mut red, mut green, mut blue) = (0, 0, 0,);
+        for j in s {
+            let j = j.split(", ");
+            for k in j {
+                let mut k = k.split(" ");
+                if let (Some(number), Some(colour)) = (k.next(), k.next()) {
+                    let number = number.parse::<u32>().unwrap();
+                    match colour {
+                        "red" => {
+                            if number > red {
+                                red = number;
+                            }
+                        }
+                        "green" => {
+                            if number > green {
+                                green = number;
+                            }
+                        }
+                        "blue" => {
+                            if number > blue {
+                                blue = number;
+                            }
+                        }
+                        _ => {}
+                    }
+                }
+            }
+        }
+        total += red*green*blue;
     }
-    let mut result = 0;
-    for (j, k) in strategy.iter() {
-        result += score(j, k);
-    }
-    println!("{:?}", result);
+    println!("{}", total);
 }
