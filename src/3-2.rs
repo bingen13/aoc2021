c4203b03dfba2b7d07e862261ef30f3883bd5c8c
modulus <modulus@spiritofcontradiction.eu>
2023-12-05 13:43:11 +0100

    AoC 2023, day 3, task 2.

--- src/3-2.rs
+++ src/3-2.rs
@@ -1,45 +1,80 @@
-use std::collections::HashSet;
+use regex::Regex;
+use std::cmp::{max, min};
 use std::fs::read_to_string;
+
+fn comdif(a: usize, b: usize) -> usize {
+    max(a, b) - min(a, b)
+}
 
 fn main() {
     let f = read_to_string("input.txt").unwrap();
     let f = f.split('\n');
-    let mut priors = 0;
-    let mut h1 = HashSet::new();
-    let mut h2 = HashSet::new();
-    let mut h3 = HashSet::new();
+    let f = f
+        .filter_map(|s| if s == "" { None } else { Some(s.to_string()) })
+        .collect::<Vec<String>>();
 
-    for (i, j) in f.enumerate() {
-        if i % 3 == 0 {
-            h1 = HashSet::new();
-            h2 = HashSet::new();
-            h3 = HashSet::new();
-        }
-        for k in j.chars() {
-            match i % 3 {
-                0 => {
-                    h1.insert(k);
-                }
-                1 => {
-                    h2.insert(k);
-                }
-                2 => {
-                    h3.insert(k);
-                }
-                _ => (),
-            }
-        }
-        if i % 3 == 2 {
-            let common = h1.iter()
-                .filter(|n| h2.contains(&n) & h3.contains(&n))
-                .next()
-                .unwrap();
-            priors += 1;
-            if common.is_uppercase() {
-                priors += 26;
-            }
-            priors += common.to_lowercase().next().unwrap() as u32 - 'a' as u32;
+    let re = Regex::new(r"(\d+)").unwrap();
+    let mut numbers = Vec::new();
+    for (i, j) in f.iter().enumerate() {
+        for k in re.captures_iter(j) {
+            numbers.push((
+                i,
+                k.get(1)
+                    .unwrap()
+                    .as_str()
+                    .to_string()
+                    .parse::<u32>()
+                    .unwrap(),
+                k.get(1).unwrap().start(),
+                k.get(1).unwrap().end(),
+            ));
         }
     }
-println!("{}", priors);
+    let l = f[0].len();
+    let mut ranges = Vec::new();
+
+    for n in numbers {
+        let (line, number, start, end) = n;
+        let mut sur = Vec::new();
+        let min = if start == 0 { 0 } else { start - 1 };
+        let max = if end == l { l } else { end + 1 };
+        if line != 0 {
+            for i in f[line - 1][min..max].chars() {
+                sur.push(i);
+            }
+        }
+        if line != l - 1 {
+            for i in f[line + 1][min..max].chars() {
+                sur.push(i);
+            }
+        }
+        if start != 0 {
+            sur.push(f[line][min..].chars().next().unwrap());
+        }
+        if end != l {
+            sur.push(f[line][..end + 1].chars().rev().next().unwrap());
+        }
+        if sur.iter().any(|&c| c != '.') {
+            ranges.push((line, start, end - 1, number));
+        }
+    }
+    let mut total = 0;
+    for (i, j) in f.iter().enumerate() {
+        for (k, l) in j.chars().enumerate() {
+            if l == '*' {
+                let result = ranges
+                    .iter()
+                    .filter(|f| {
+                        comdif(f.0, i) <= 1
+                            && ((f.1..=f.2).contains(&k) || (k == f.2 + 1) || k == f.1 - 1)
+                    })
+                    .map(|x| x.3)
+                    .collect::<Vec<u32>>();
+                if result.len() == 2 {
+                    total += result[0] * result[1];
+                }
+            }
+        }
+    }
+    println!("{}", total);
 }
