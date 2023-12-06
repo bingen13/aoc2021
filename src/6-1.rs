ad02eabd35b667ed4431ae1b70984d4cdcadc145
modulus <modulus@spiritofcontradiction.eu>
2023-12-06 17:58:03 +0100

    AoC 2023, day 6, task 1.

--- src/6-1.rs
+++ src/6-1.rs
@@ -1,21 +1,26 @@
-use std::collections::HashSet;
+use regex::Regex;
 use std::fs::read_to_string;
 
 fn main() {
     let f = read_to_string("input.txt").unwrap();
-    let mut v = Vec::new();
-    for i in f.chars() {
-        v.push(i);
-    }
-    for i in 3..v.len() {
-        let mut h = HashSet::new();
-        h.insert(v[i - 3]);
-        h.insert(v[i - 2]);
-        h.insert(v[i - 1]);
-        h.insert(v[i]);
-        if h.len() == 4 {
-            println!("{}", i + 1);
-            break;
+    let mut f = f.split('\n');
+    let re = Regex::new(r"\d+").unwrap();
+    let time = re
+        .find_iter(f.next().unwrap())
+        .map(|x| x.as_str().parse::<u32>().unwrap())
+        .collect::<Vec<_>>();
+    let distance = re
+        .find_iter(f.next().unwrap())
+        .map(|x| x.as_str().parse::<u32>().unwrap())
+        .collect::<Vec<_>>();
+    let l = time.len();
+    let mut total = Vec::new();
+    for i in 0..l {
+        let mut j = 0;
+        while (time[i] - j) * j <= distance[i] {
+            j += 1;
         }
+        total.push((time[i] + 1) - (j * 2));
     }
+    println!("{}", total.iter().fold(1, |x, y| x * y));
 }
