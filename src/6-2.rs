488001905228071e1337f05f6761e2b9ad2e6ff4
modulus <modulus@spiritofcontradiction.eu>
2023-12-06 18:09:23 +0100

    AoC 2023, day 6, task 2.

--- src/6-2.rs
+++ src/6-2.rs
@@ -1,31 +1,26 @@
-use std::collections::HashSet;
+use regex::Regex;
 use std::fs::read_to_string;
 
 fn main() {
     let f = read_to_string("input.txt").unwrap();
-    let mut v = Vec::new();
-    for i in f.chars() {
-        v.push(i);
+    let mut f = f.split('\n');
+    let re = Regex::new(r"\d+").unwrap();
+    let time = re
+        .find_iter(f.next().unwrap())
+        .map(|x| x.as_str())
+        .collect::<String>()
+        .parse::<u64>()
+        .unwrap();
+    let distance = re
+        .find_iter(f.next().unwrap())
+        .map(|x| x.as_str())
+        .collect::<String>()
+        .parse::<u64>()
+        .unwrap();
+    let mut j = 0;
+    while (time - j) * j <= distance {
+        j += 1;
     }
-    for i in 13..v.len() {
-        let mut h = HashSet::new();
-        h.insert(v[i - 13]);
-        h.insert(v[i - 12]);
-        h.insert(v[i - 11]);
-        h.insert(v[i - 10]);
-        h.insert(v[i - 9]);
-        h.insert(v[i - 8]);
-        h.insert(v[i - 7]);
-        h.insert(v[i - 6]);
-        h.insert(v[i - 5]);
-        h.insert(v[i - 4]);
-        h.insert(v[i - 3]);
-        h.insert(v[i - 2]);
-        h.insert(v[i - 1]);
-        h.insert(v[i]);
-        if h.len() == 14 {
-            println!("{}", i + 1);
-            break;
-        }
-    }
+    let total = (time + 1) - (j * 2);
+    println!("{}", total);
 }
