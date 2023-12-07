2ba0f2830e4f2174e516ec18ddfa495309758f8e
modulus <modulus@spiritofcontradiction.eu>
2023-12-07 04:41:00 +0100

    AoC 2023, Day 5, Task 1.

--- src/5-1.rs
+++ src/5-1.rs
@@ -1,40 +1,43 @@
+use regex::Regex;
+use std::collections::HashMap;
 use std::fs::read_to_string;
 
 fn main() {
     let f = read_to_string("input.txt").unwrap();
     let mut f = f.split("\n\n");
-    let stacks = f.next().unwrap();
-    let moves = f.next().unwrap();
-    let mut blocks = Vec::new();
-    for _i in 0..9 {
-        blocks.push(Vec::new());
-    }
+    let re = Regex::new(r"\d+").unwrap();
+    let seeds = re
+        .find_iter(f.next().unwrap())
+        .map(|x| x.as_str().parse::<u64>().unwrap())
+        .collect::<Vec<_>>();
+    let mut ranges = Vec::new();
 
-    for i in stacks.split('\n') {
-        for (j, k) in i.chars().enumerate() {
-            if (j % 4 == 1) & !k.is_numeric() & (k != ' ') {
-                blocks[j / 4].push(k);
+    for i in f {
+        if i.is_empty() {
+            break;
+        }
+        let mut r = HashMap::new();
+        for j in i.split('\n') {
+            if let [dest, source, lenght] = re
+                .find_iter(j)
+                .map(|x| x.as_str().parse::<u64>().unwrap())
+                .collect::<Vec<_>>()[..]
+            {
+                r.insert(source..(source + lenght), dest);
             }
         }
+        ranges.push(r.clone());
     }
-    for i in 0..blocks.len() {
-        blocks[i].reverse();
-    }
-    for i in moves.split('\n') {
-        if !i.is_empty() {
-            let mut i = i.split(' ');
-            let n1 = i.nth(1).unwrap().parse::<usize>().unwrap();
-            let n2 = i.nth(1).unwrap().parse::<usize>().unwrap();
-            let n3 = i.nth(1).unwrap().parse::<usize>().unwrap();
-            for _ in 0..n1 {
-                let elem = blocks[n2 - 1].pop().unwrap();
-                blocks[n3 - 1].push(elem);
+    let mut locations = Vec::new();
+
+    for i in seeds {
+        let mut location = i;
+        for j in &ranges {
+            if let Some((key, value)) = j.iter().find(|(k, _)| k.contains(&location)) {
+                location = value + (location - key.start);
             }
         }
+        locations.push(location);
     }
-    let mut result = String::new();
-    for i in 0..blocks.len() {
-        result.push(blocks[i].pop().unwrap());
-    }
-    println!("{}", result);
+    println!("{}", locations.iter().min().unwrap());
 }
