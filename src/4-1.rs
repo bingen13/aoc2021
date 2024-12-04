3a39b841301da2a1a2d9fafd8520dd24b6e78f9e
modulus <modulus@spiritofcontradiction.eu>
2024-12-04 10:04:40 +0100

    Aoc24, day 4, tasks 1 and 2.

--- src/4-1.rs
+++ src/4-1.rs
@@ -1,30 +1,54 @@
-use regex::Regex;
-use std::collections::HashSet;
 use std::fs::read_to_string;
 
 fn main() {
+    let mut letters = Vec::new();
     let f = read_to_string("input.txt").unwrap();
-    let f = f.split('\n');
-    let re = Regex::new(r"\d+").unwrap();
-
-    let mut total = 0;
-    for i in f {
+    let mut s = f.split('\n');
+    while let Some(i) = s.next() {
         if i.is_empty() {
             break;
         }
-        let mut n = i.split(": ").nth(1).unwrap().split('|');
-        let numbers1 = re
-            .find_iter(n.next().unwrap())
-            .map(|x| x.as_str().parse::<u32>().unwrap())
-            .collect::<HashSet<_>>();
-        let numbers2 = re
-            .find_iter(n.next().unwrap())
-            .map(|x| x.as_str().parse::<u32>().unwrap())
-            .collect::<HashSet<_>>();
-        let same= numbers1.intersection(&numbers2).count();
-        if same > 0 {
-            total += 2_u32.pow(same as u32 - 1);
-        }
+        letters.push(i.to_string());
     }
-    println!("{}", total);
+    let key = "XMAS";
+    let mut matches = 0;
+    for r in 0..4 {
+        let mut x: Vec<(usize, usize)> = Vec::new();
+        for (i, val) in letters.iter().enumerate() {
+            for (j, val) in val.chars().enumerate() {
+                if val == 'X' {
+                    x.push((i, j));
+                }
+            }
+        }
+        for (p1, p2) in &x {
+            // Horizontals.
+            if (*p2 <= letters.len() - 4) && (letters[*p1][*p2..(*p2 + 4)] == *key) {
+                matches += 1;
+            }
+            // Diagonals.
+            if (*p1 <= letters.len() - 4)
+                && (*p2 <= letters.len() - 4)
+                && ((0..4)
+                    .map(|c| letters[p1 + c].chars().nth(p2 + c).unwrap())
+                    .collect::<String>()
+                    .as_str()
+                    == key)
+            {
+                matches += 1;
+            }
+        }
+        // Rotate 90 degrees.
+        let l = letters.len();
+        let mut v = Vec::new();
+        for i in 0..l {
+            let mut v2 = Vec::new();
+            for j in 0..l {
+                v2.push(letters[j].chars().nth(l - i - 1).unwrap());
+            }
+            v.push(v2.into_iter().collect::<String>());
+        }
+        letters = v;
+    }
+    println!("{}", matches);
 }
--- src/4-2.rs
+++ src/4-2.rs
@@ -1,43 +1,50 @@
-use regex::Regex;
-use std::collections::{HashMap, HashSet};
 use std::fs::read_to_string;
 
 fn main() {
+    let mut letters = Vec::new();
     let f = read_to_string("input.txt").unwrap();
-    let f = f.split('\n');
-    let re = Regex::new(r"\d+").unwrap();
-
-    let mut cards = HashMap::new();
-    for i in 0..f.clone().count() - 1 {
-        cards.insert(i, 1);
-    }
-
-    for (j, i) in f.enumerate() {
+    let mut s = f.split('\n');
+    while let Some(i) = s.next() {
         if i.is_empty() {
             break;
         }
-        let mut n = i.split(": ").nth(1).unwrap().split('|');
-        let numbers1 = re
-            .find_iter(n.next().unwrap())
-            .map(|x| x.as_str().parse::<u32>().unwrap())
-            .collect::<HashSet<_>>();
-        let numbers2 = re
-            .find_iter(n.next().unwrap())
-            .map(|x| x.as_str().parse::<u32>().unwrap())
-            .collect::<HashSet<_>>();
-        let same = numbers1.intersection(&numbers2).count();
-        let multiplier = cards.get(&j).unwrap().clone();
-        for k in 0..same {
-            if cards.contains_key(&(k + j + 1)) {
-                cards.entry(&k + j + 1).and_modify(|c| *c += multiplier);
+        letters.push(i.to_string());
+    }
+    let key = "XMAS";
+    let mut matches = 0;
+    for r in 0..4 {
+        let mut a: Vec<(usize, usize)> = Vec::new();
+        for (i, val) in letters.iter().enumerate() {
+            for (j, val) in val.chars().enumerate() {
+                if val == 'A' {
+                    a.push((i, j));
+                }
             }
         }
+        let l = letters.len();
+        for (p1, p2) in &a {
+            if (*p1 > 0)
+                && (*p2 > 0)
+                && (*p1 < l - 1)
+                && (*p2 < l - 1)
+                && (letters[p1 - 1].chars().nth(p2 - 1).unwrap() == 'M')
+                && (letters[p1 - 1].chars().nth(p2 + 1).unwrap() == 'S')
+                && (letters[p1 + 1].chars().nth(p2 - 1).unwrap() == 'M')
+                && (letters[p1 + 1].chars().nth(p2 + 1).unwrap() == 'S')
+            {
+                matches += 1;
+            }
+        }
+        // Rotate 90 degrees.
+        let mut v = Vec::new();
+        for i in 0..l {
+            let mut v2 = Vec::new();
+            for j in 0..l {
+                v2.push(letters[j].chars().nth(l - i - 1).unwrap());
+            }
+            v.push(v2.into_iter().collect::<String>());
+        }
+        letters = v;
     }
-/*
-    for i in 0..cards.keys().count() {
-        println!("{}: {}", i, cards.get(&i).unwrap());
-    }
-*/
-
-    println!("{}", cards.values().sum::<usize>());
+    println!("{}", matches);
 }
