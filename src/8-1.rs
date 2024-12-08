3f8dfefcc58d4aa5733fb8a250d86b03a9d120f6
david <modulus@spiritofcontradiction.eu>
2024-12-08 10:18:25 +0100

    AoC24, day 8, task 1.

--- src/8-1.rs
+++ src/8-1.rs
@@ -1,76 +1,51 @@
+use std::collections::HashSet;
 use std::fs::read_to_string;
 
 fn main() {
     let f = read_to_string("input.txt").unwrap();
+    let l = f.split('\n').count() - 1;
+    let mut m = Vec::new();
     let f = f.split('\n');
-    let mut trees = Vec::new();
-    let mut lines = 0;
-    for (i, j) in f.enumerate() {
-        if j.is_empty() {
-            break;
-        }
-        lines = i;
-        for c in j.chars() {
-            trees.push(c.to_digit(10).unwrap());
+    for (i, line) in f.enumerate() {
+        for (j, ch) in line.chars().enumerate() {
+            if ch.is_alphanumeric() {
+                m.push((ch, (i, j)));
+            }
         }
     }
-    lines += 1;
-    let length = trees.len();
-    let rows = length / lines;
-
-    let mut visible = 0;
-
-    for (i, j) in trees.iter().enumerate() {
-        if i < rows {
-            visible += 1;
-        } else if i >= length - rows {
-            visible += 1;
-        } else if i % rows == 0 {
-            visible += 1;
-        } else if i % rows == rows - 1 {
-            visible += 1;
-        } else if trees
+    let h: HashSet<_> = m.iter().map(|e| e.0).collect();
+    let mut anti = HashSet::new();
+    'outer: for i in h {
+        let mut v = m
             .iter()
-            .enumerate()
-            .filter(|(n, _)| (n < &i) & ((n % rows) == (i % rows)))
-            .max_by(|m1, m2| m1.1.cmp(m2.1))
-            .unwrap()
-            .1
-            < &j
-        {
-            visible += 1;
-        } else if trees
-            .iter()
-            .enumerate()
-            .filter(|(n, _)| (n > &i) & ((n % rows) == (i % rows)))
-            .max_by(|m1, m2| m1.1.cmp(m2.1))
-            .unwrap()
-            .1
-            < &j
-        {
-            visible += 1;
-        } else if trees
-            .iter()
-            .enumerate()
-            .filter(|(n, _)| (n >= &(rows * (i / rows))) & (n < &i))
-            .max_by(|m1, m2| m1.1.cmp(m2.1))
-            .unwrap()
-            .1
-            < &j
-        {
-            visible += 1;
-        } else if trees
-            .iter()
-            .enumerate()
-            .filter(|(n, _)| (n > &i) & (n < &(rows * ((i / rows) + 1))))
-            .max_by(|m1, m2| m1.1.cmp(m2.1))
-            .unwrap()
-            .1
-            < &j
-        {
-            visible += 1;
+            .filter_map(|e| if e.0 != i { None } else { Some(e.1) })
+            .collect::<Vec<_>>();
+        loop {
+            let e = v.remove(0);
+            if v.len() < 1 {
+                continue 'outer;
+            }
+            for j in &v {
+                if e.1 <= j.1 {
+                    if (e.0 >= (j.0 - e.0)) && (e.1 >= (j.1 - e.1)) {
+                        anti.insert((e.0 - (j.0 - e.0), e.1 - (j.1 - e.1)));
+                    }
+                    anti.insert((j.0 + (j.0 - e.0), j.1 + (j.1 - e.1)));
+                } else {
+                    if (e.0 >= (j.0 - e.0)) {
+                        anti.insert((e.0 - (j.0 - e.0), e.1 + (e.1 - j.1)));
+                    }
+                    if (j.1 >= (e.1 - j.1)) {
+                        anti.insert((j.0 + (j.0 - e.0), j.1 - (e.1 - j.1)));
+                    }
+                }
+            }
         }
     }
-
-    println!("{}", visible);
+    println!(
+        "{}",
+        anti.iter()
+            .filter(|(x1, x2)| (*x1 < l) && (*x2 < l))
+            .count()
+    );
 }
