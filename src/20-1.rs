03151cff129eb9828f197e59458d6205a5ca0c30
modulus <modulus@spiritofcontradiction.eu>
2025-01-09 14:02:05 +0100

    AoC24, day 20, tasks 1 and 2.

--- src/20-1.rs
+++ src/20-1.rs
@@ -1,40 +1,143 @@
+use std::cmp::{max, min};
+use std::collections::{HashMap, HashSet};
 use std::fs::read_to_string;
 
-fn mix(v: &mut Vec<i64>, p: &mut Vec<usize>) {
-    for i in 0..v.len() {
-        let item = p.iter().position(|&x| x == i).unwrap();
-        p.remove(item);
-        p.insert((item as i64 + v[i]).rem_euclid(p.len() as i64) as usize, i);
+#[derive(Debug, Copy, Clone, Hash, PartialEq, Eq)]
+struct Point {
+    x: usize,
+    y: usize,
+}
+
+impl Point {
+    fn north(&self) -> Option<Self> {
+        if self.y > 0 {
+            Some(Point {
+                x: self.x,
+                y: self.y - 1,
+            })
+        } else {
+            None
+        }
+    }
+    fn south(&self) -> Option<Self> {
+        if self.y < 140 {
+            Some(Point {
+                x: self.x,
+                y: self.y + 1,
+            })
+        } else {
+            None
+        }
+    }
+    fn west(&self) -> Option<Self> {
+        if self.x > 0 {
+            Some(Point {
+                x: self.x - 1,
+                y: self.y,
+            })
+        } else {
+            None
+        }
+    }
+    fn east(&self) -> Option<Self> {
+        if self.x < 140 {
+            Some(Point {
+                x: self.x + 1,
+                y: self.y,
+            })
+        } else {
+            None
+        }
     }
 }
 
-fn order(v: &[(usize, i64)]) -> Vec<i64> {
-    const key: i64 = 811589153;
-    let mut result = Vec::new();
-    for i in 0..v.len() {
-        result.push(v.iter().find(|p| p.0 == i).unwrap().1);
+fn dist(p1: &Point, p2: &Point) -> usize {
+    (max(p1.x, p2.x) - min(p1.x, p2.x)) + (max(p1.y, p2.y) - min(p1.y, p2.y))
+}
+
+fn length(
+    start: &Point,
+    end: &Point,
+    walls: &HashSet<Point>,
+    distance: &mut HashMap<Point, usize>,
+    pred: &mut HashMap<Point, Point>,
+) -> usize {
+    let mut visited: HashSet<Point> = HashSet::new();
+    let mut unvisited = HashSet::new();
+    distance.insert(*start, 0);
+    unvisited.insert(*start);
+    while let Some(node) = unvisited
+        .iter()
+        .min_by(|a, b| distance.get(a).cmp(&distance.get(b)))
+    {
+        let node = ((*node).clone(), distance.get(node).unwrap().clone());
+        let neighbours = [node.0.north(), node.0.south(), node.0.east(), node.0.west()]
+            .into_iter()
+            .flatten()
+            .map(|x| (x, node.1 + 1))
+            .filter(|x| !walls.contains(&x.0))
+            .collect::<Vec<_>>();
+        for n in neighbours.into_iter() {
+            if !visited.contains(&n.0) {
+                unvisited.insert(n.0);
+                match distance.get(&n.0) {
+                    None => {
+                        pred.insert(n.0, node.0);
+                        distance.insert(n.0, n.1);
+                    }
+                    Some(dist) if *dist > n.1 => {
+                        pred.insert(n.0, node.0);
+                        distance.insert(n.0, n.1);
+                    }
+                    Some(_) => (),
+                };
+            }
+        }
+        visited.insert(node.0);
+        unvisited.remove(&node.0);
     }
-    result
+    *distance.get(&end).unwrap()
 }
 
 fn main() {
     let f = read_to_string("input.txt").unwrap();
-    let f = f.split('\n');
-    let mut numbers = Vec::new();
-    for i in f {
-        if let Ok(i) = i.parse::<i64>() {
-            numbers.push(i);
+    let mut start = Point { x: 0, y: 0 };
+    let mut end = Point { x: 70, y: 70 };
+    let mut walls: HashSet<Point> = HashSet::new();
+    for (i, line) in f.lines().enumerate() {
+        for (j, c) in line.chars().enumerate() {
+            match c {
+                '#' => {
+                    walls.insert(Point { x: j, y: i });
+                }
+                '.' => (),
+                'S' => start = Point { x: j, y: i },
+                'E' => end = Point { x: j, y: i },
+                _ => unimplemented!(),
+            }
         }
     }
-    let mut positions: Vec<_> = (0..numbers.len()).collect();
-    mix(&mut numbers, &mut positions);
-    let p = positions
-        .iter()
-        .position(|&x| x == numbers.iter().position(|&x| x == 0).unwrap())
-        .unwrap();
-    let pos1 = numbers[positions[(p + 1000) % positions.len()]];
-    let pos2 = numbers[positions[(p + 2000) % positions.len()]];
-    let pos3 = numbers[positions[(p + 3000) % positions.len()]];
-    let result = pos1 + pos2 + pos3;
-    println!("{}", result);
+    let mut distance = HashMap::new();
+    let mut pred: HashMap<Point, Point> = HashMap::new();
+    let _ = length(&end, &start, &walls, &mut distance, &mut pred);
+    let mut path = Vec::new();
+    path.push(start);
+    while let Some(n) = pred.get(path.last().unwrap()) {
+        path.push(*n);
+    }
+    let mut c = 0;
+    for p in path {
+        let d = *distance.get(&p).unwrap();
+        if d < 100 {
+            continue;
+        }
+        let cans = distance.iter().filter(|(k, _)| dist(&p, k) < 3);
+        for bestplace in cans {
+            let mindist = dist(&p, &bestplace.0) + bestplace.1;
+            if (mindist < d) && (d - mindist >= 100) {
+                c += 1;
+            }
+        }
+    }
+    println!("{}", c);
 }
